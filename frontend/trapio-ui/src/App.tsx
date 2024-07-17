d4a6695deb06617caba5d94c4f911cf56ff6909b
Josh Hamill <joshua@transposit.com>
2024-07-17 16:28:38 -0400

    feat(ui): stub out react code with rsbuild

---
+++ frontend/trapio-ui/src/App.tsx
@@ -0,0 +1,8 @@
+export const App = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <p>Start building amazing things with Rsbuild.</p>
+    </div>
+  );
+};
